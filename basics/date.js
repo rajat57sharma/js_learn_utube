31aafb118d22fe33f8364363fd184080466fd914
rajat57sharma <69747891+rajat57sharma@users.noreply.github.com>
2023-11-22 07:25:19 +0000

    date

---
+++ basics/date.js
@@ -0,0 +1,14 @@
+const d = new Date()
+
+// console.log(d)
+// console.log(d.toString())
+// console.log(typeof d)
+
+const createdDate = new Date(2023, 0, 2)
+// console.log(createdDate.toDateString())
+
+let timestamp = Date.now()
+//console.log(timestamp)
+
+// to milisecond
+console.log(Math.round(timestamp/1000))
