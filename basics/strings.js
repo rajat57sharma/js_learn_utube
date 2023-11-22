0ab1c4635ee7fbee72a48209d4e2d1b55357e20a
rajat57sharma <69747891+rajat57sharma@users.noreply.github.com>
2023-11-22 07:27:07 +0000

    strings

---
+++ basics/strings.js
@@ -0,0 +1,12 @@
+const name = "rajat"
+const countr = 50
+
+const newname = new String('hello')
+
+console.log(newname)
+
+//console.log(name + countr + " value")  dont use this sintax instead go for string interpolation
+
+console.log(`my name is ${name} and repo count is ${countr}`) // 
+
+// go to console and check prototype their
